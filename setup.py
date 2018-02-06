602d6ece3ce2a52d8b288e79ff4b25435941c5a7
Jaume Florez <jflorez@gisce.net>
2018-02-06 08:52:46 +0100

    Add basic setup for package

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='powerp-translator',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/gisce/destral',
+    install_requires=[
+        'osconf',
+        'mamba',
+        'coverage',
+        'babel>=2.4.0'
+    ],
+    license='GNU GPLv3',
+    author='GISCE-TI, S.L.',
+    author_email='devel@gisce.net',
+    description='OpenERP external translation framework'
+)
