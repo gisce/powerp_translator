4f0c98b7e17ed8d1b8d002b5871cba8a7e450f96
Jaume Florez <jflorez@gisce.net>
2018-02-16 15:45:58 +0100

    Correctly log all from translator

--- powerp_translator/cli.py
+++ powerp_translator/cli.py
@@ -8,10 +8,10 @@ import time
 import logging
 import osconf
 import click
+import sql_db
 
 
 def createdb(dbname=False):
-    import sql_db
     conn = sql_db.db_connect('postgres')
     cursor = conn.cursor()
     if not dbname:
@@ -23,6 +23,31 @@ def createdb(dbname=False):
     finally:
         cursor.close()
         sql_db.close_db('postgres')
+
+def initialize_logger(verbosity):
+    logging.basicConfig(
+        format='[%(asctime)s](%(name)s) %(levelname)s: %(message)s',
+        datefmt='%Y/%m/%d-%H:%M:%S',
+        # Set custom level to avoid disabling from OpenERPService
+        level=(verbosity + logging.CRITICAL) 
+    )
+    logger = logging.getLogger('TranslationExport')
+    logger.addLevelName(logging.INFO + logging.CRITICAL, 'INFO')
+    logger.addLevelName(logging.DEBUG + logging.CRITICAL, 'DEBUG')
+    logger.addLevelName(logging.ERROR + logging.CRITICAL, 'ERROR')
+
+def log_info(msg):
+    logger = logging.getLogger('TranslationExport')
+    logger.log(logging.INFO + logging.CRITICAL, msg)
+
+def log_debug(msg):
+    logger = logging.getLogger('TranslationExport')
+    logger.log(logging.DEBUG + logging.CRITICAL, msg)
+
+def log_error(msg):
+    logger = logging.getLogger('TranslationExport')
+    logger.log(logging.ERROR + logging.CRITICAL, msg)
+
 
 @click.command(context_settings=dict(
     ignore_unknown_options=True,
@@ -38,51 +63,57 @@ def createdb(dbname=False):
 def transexport(modules=[], verbose=False, debug=False):
     verbosity = logging.DEBUG if debug else (
         logging.INFO if verbose else logging.WARNING)
-    logging.basicConfig(
-        format='[%(asctime)s](%(name)s) %(levelname)s: %(message)s',
-        datefmt='%Y/%m/%d-%H:%M:%S',
-        level=verbosity
-    )
-    logger = logging.getLogger('TranslationExport')
-    logger.info('Running with verbosity: {}'.format('DEBUG' if debug else 'INFO'))
+    initialize_logger(verbosity)    
+    log_info('Running with verbosity: {}'.format('DEBUG' if debug else 'INFO'))
     if modules:
-        logger.debug('Modules: {}'.format(modules))
+        log_debug('Modules: {}'.format(modules))
     else:
-        logger.error('No Modules to export!')
+        log_error('No Modules to export!')
         exit(-1)
-    logger.info('Starting OpenERP Service')
-    logger.debug('Checking for OpenERP env vars')
+    log_info('Starting OpenERP Service')
+    log_debug('Checking for OpenERP env vars')
     dbname = 'translate_{}'.format(str(int(time.time())))
-    confs = osconf.config_from_environment(
-        'OPENERP',
-        ['addons_path', 'root_path', 'db_user', 'db_password', 'db_name'],
-        db_name=dbname
-    )
+    try:
+        confs = osconf.config_from_environment(
+            'OPENERP',
+            ['addons_path', 'root_path', 'db_user', 'db_password', 'db_name'],
+            db_name=dbname
+        )
+    except:
+        log_error(
+            'Please, make sure you provide the following environment vars:'
+            '\n\tOPENERP_ADDONS_PATH'
+            '\n\tOPENERP_ROOT_PATH'
+            '\n\tOPENERP_DB_USER'
+            '\n\tOPENERP_DB_PASSWORD'
+        )
+        exit(-1)
     dbname = confs['db_name']
     environ['OPENERP_DB_NAME'] = dbname
-    logger.debug('DB_NAME: {}'.format(dbname))
+    log_debug('DB_NAME: {}'.format(dbname))
     addons_path = confs['addons_path']
-    logger.debug('ADDONS_PATH: {}'.format(addons_path))
+    log_debug('ADDONS_PATH: {}'.format(addons_path))
     root_path = confs['root_path']
-    logger.debug('ROOT_PATH: {}'.format(root_path))
+    log_debug('ROOT_PATH: {}'.format(root_path))
     try:
         oerp_service = OpenERPService()
-        print('Using database: {}'.format(dbname))
+        log_info('Using database: {}'.format(dbname))
     except:
-        print('Creating database: {}'.format(dbname))
+        log_info('Creating database: {}'.format(dbname))
         createdb(dbname=dbname)
+        log_debug('Initialize OpenERPService')
         oerp_service = OpenERPService()
     for module_name in modules:
-        print('Exporting module {}'.format(module_name))
+        log_info('Exporting module "{}"'.format(module_name))
         if module_name not in listdir(addons_path):
-            print(
-                'Module {} not found in addons path!'.format(module_name))
+            log_error(
+                'Module "{}" not found in addons path!'.format(module_name)
+            )
             continue
         try:
             with Transaction().start(
                 database_name=dbname
             ) as temp:
-                logger = logging.getLogger('TranslationExport')
                 temp.service.install_module(module_name)
                 uid = temp.user
                 pool = temp.pool
@@ -98,7 +129,7 @@ def transexport(modules=[], verbose=False, debug=False):
                         ''.format(module_name))
                     module_id = module_id[0]
                 if not len(module_id):
-                    print('Module "{}" not found with this name'.format(
+                    log_error('Module "{}" not found with this name'.format(
                         module_name
                     ))
                     continue
@@ -116,10 +147,13 @@ def transexport(modules=[], verbose=False, debug=False):
                 ) as potfile:
                     potfile.write(decodestring(wizard.data))
         except Exception as err:
-            print(
-                'Couldn\'t export translations for module {}'.format(err))
+            log_error(
+                'Couldn\'t export translations for module "{}": {}'.format(
+                    module_name, err
+                )
+            )
             continue
-    print('Removing temp database {}'.format(dbname))
+    log_info('Removing temp database {}'.format(dbname))
     oerp_service.drop_database()
 
 
